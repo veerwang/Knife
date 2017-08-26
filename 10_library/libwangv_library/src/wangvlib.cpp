3c2ae0e393adafa38560b001389070698999fa80
kevin <kevin.wang2004@hotmail.com>
2017-08-26 21:25:09 +0800

    添加只有库的试验

---
+++ 10_library/libwangv_library/src/wangvlib.cpp
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  wangvlib.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年12月02日 09时21分46秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin (WangWei), kevin.wang2004@hotmail.com
+ *        Company:  LiLin-Tech
+ *   Organization:  GNU
+ *
+ * =====================================================================================
+ */
+
+#include "wangvlib.h"
+#include <cstdlib>
+#include <cstdio>
+
+namespace WangV {
+static const char PROJ_VERSION[] = {"V1.00"};
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  get_version 
+ *  Description:  获得模块的版本信息 
+ * =====================================================================================
+ */
+const char* get_version()
+{
+	return PROJ_VERSION;
+}
+}
---
+++ 10_library/test/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/26/2017 09:25:35 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin (WangWei), wwei@landicorp.com
+ *        Company:  Landicorp
+ *   Organization:  GNU
+ *
+ * =====================================================================================
+ */
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+
+#include <wangvlib.h> 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	std::cout<<WangV::get_version()<<std::endl;
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
