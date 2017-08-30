f6fb0e64e4ca3c3e7e46d444d0b3f22d6d5e4fc5
kevin.wang <wwei@landicorp.com>
2017-08-30 19:06:28 +0800

    修正日志库，添加启动信息打印

--- 10_library/libwangv_library/src/displaylog.cpp
+++ 10_library/libwangv_library/src/displaylog.cpp
@@ -23,14 +23,14 @@
 #include <stdarg.h> //va_start va_end
 
 namespace WangV {
-static const char* FG_WHITE  	= 	"\x1B[0;m"	;   /* white */
-static const char* FG_RED    	= 	"\033[0;31m"	;   /* 0 -> normal ; 31 -> red */
-static const char* FG_RED_BOLD  =	"\033[1;31m" 	;   /* 1 -> bold ; 31 -> red */
-static const char* FG_GREEN    	=	"\033[0;32m"  	;   /* 4 -> underline ; 32 -> green */
-#define FG_GREEN_BOLD   "\033[1;32m"
-#define FG_YELLOW 	"\033[0;33m"  /* 0 -> normal ; 33 -> yellow */
-#define FG_YELLOW_BOLD	"\033[1;33m"
-#define FG_BLUE     	"\033[0;34m"  /* 9 -> strike ; 34 -> blue */
+static const char* FG_WHITE  		= 	"\x1B[0;m"	;   /* white */
+static const char* FG_RED    		= 	"\033[0;31m"	;   /* 0 -> normal ; 31 -> red */
+static const char* FG_RED_BOLD  	=	"\033[1;31m" 	;   /* 1 -> bold ; 31 -> red */
+static const char* FG_GREEN    		=	"\033[0;32m"  	;   /* 4 -> underline ; 32 -> green */
+static const char* FG_GREEN_BOLD	=       "\033[1;32m"	;
+static const char* FG_YELLOW 		=       "\033[0;33m"  	;   /* 0 -> normal ; 33 -> yellow */
+static const char* FG_YELLOW_BOLD	=	"\033[1;33m"	;
+static const char* FG_BLUE     		=	"\033[0;34m"  	;/* 9 -> strike ; 34 -> blue */
 #define FG_BLUE_BOLD    "\033[1;34m"
 #define FG_CYAN     	"\033[0;36m" /* 0 -> normal ; 36 -> cyan */
 #define FG_CYAN_BOLD	"\033[0;36m"
@@ -41,11 +41,11 @@ static const char* FG_GREEN    	=	"\033[0;32m"  	;   /* 4 -> underline ; 32 -> g
 #define BG_DEFAULT  	"\033[49m"
 #define RESET_COLOR    	"\033[0m" /* to flush the previous property */
 
-#define LOG_INFO 	(1<<0) //1
-#define LOG_WARN 	(1<<1) //2
-#define LOG_ERROR 	(1<<2) //4
-#define LOG_DEBUG 	(1<<3) //8
-#define LOG_ALL 	((2<<16)-1)
+#define LOG_ALL		1
+#define LOG_ERROR	2
+#define LOG_WARN	3 
+#define LOG_DEBUG	4
+#define LOG_INFO	5
 
 #define INFO(f...)  do { LOG_WRITER(LOG_INFO, __FILE__, __FUNCTION__, __LINE__, f); } while (0)
 #define WARN(f...)  do { LOG_WRITER(LOG_WARN, __FILE__, __FUNCTION__, __LINE__, f); } while (0)
@@ -56,7 +56,7 @@ static const char* FG_GREEN    	=	"\033[0;32m"  	;   /* 4 -> underline ; 32 -> g
 #define LOG_MAX_MSG_LEN 4098
 
 static FILE *log_device_fp = NULL;
-static int  log_level = 1;
+static int  log_level = 5;
 
 static void LOG_ADD(int level, const char *file, const char * func, int line, const char *msg);
 
@@ -73,7 +73,7 @@ const char* LOG_SET_COLOR(int level, int is_end)
 			switch (level) {
 				case LOG_INFO : return FG_DEFAULT; break;
 				case LOG_WARN : return FG_YELLOW; break;
-				case LOG_ERROR : return FG_RED; break;
+				case LOGERROR : return FG_RED; break;
 				case LOG_DEBUG : return FG_CYAN; break;
 				default : return FG_BLUE_BOLD; break;
 			}
@@ -84,7 +84,7 @@ const char* LOG_SET_COLOR(int level, int is_end)
 
 void log_module_destroy()
 {
-	if (log_device_fp != stdout) fclose(_20131025_log_device);
+	if (log_device_fp != stdout) fclose(log_device_fp);
 }
 
 void log_module_level(int level)
@@ -94,13 +94,13 @@ void log_module_level(int level)
 
 static void LOG_ADD(int level, const char *file, const char * func, int line, const char *msg)
 {
-    const char *c = "IWEDX";
-    const char *datetime_format = "%Y-%m-%d %H:%M:%S";
-    time_t meow = time( NULL );
-    char buf[64];
-    int cl = 0;
+	const char *c = "IWEDX";
+	const char *datetime_format = "%Y-%m-%d %H:%M:%S";
+	time_t meow = time( NULL );
+	char buf[64];
+	int cl = 0;
 
-    if ((level & ~log_level) != 0 ) return;
+	if ( level < log_level ) return;	
 	//cl = floor(log(level)/log(2)) + 1;
 	switch (level) {
 		case LOG_INFO : cl = 0; break;
@@ -109,9 +109,9 @@ static void LOG_ADD(int level, const char *file, const char * func, int line, co
 		case LOG_DEBUG : cl = 3; break;
 		default : cl = 4; break;
 	}	
-	
-    strftime( buf, sizeof(buf), datetime_format, localtime(&meow) );
-    fprintf(_20131025_log_device, "%s%s[%d][%s(%s):%d] %c, %s%s\n",LOG_SET_COLOR(level, 0) , buf, (int)getpid(), file, func, line, c[cl], msg ,LOG_SET_COLOR(level, 1));
+
+	strftime( buf, sizeof(buf), datetime_format, localtime(&meow) );
+	fprintf(log_device_fp, "%s%s[%d][%s(%s):%d] %c, %s%s\n",LOG_SET_COLOR(level, 0) , buf, (int)getpid(), file, func, line, c[cl], msg ,LOG_SET_COLOR(level, 1));
 }
 
 void log_module_write(int level, const char *file, const char * func, int line, const char *fmt, ...)
--- 10_library/libwangv_library/src/wangvlib.cpp
+++ 10_library/libwangv_library/src/wangvlib.cpp
@@ -19,6 +19,9 @@
 
 #include "wangvlib.h"
 
+#include <cstdlib>
+#include <cstdio>
+
 namespace WangV {
 static const char PROJ_VERSION[] = {"V1.01"};
 
@@ -34,5 +37,17 @@ const char* get_version()
 	return PROJ_VERSION;
 }
 
+void __attribute__ ((constructor)) init_module()
+{
+	WangV::log_module_init(NULL);
+	WangV::log_module_level(4);
+	WangV::log_module_write(4,__FILE__,__FUNCTION__,__LINE__,"init module %s","kevin");
+}
+
+void __attribute__ ((destructor)) release_module()
+{
+	WangV::log_module_write((4),__FILE__,__FUNCTION__,__LINE__,"destruct module %s","kevin");
+	WangV::log_module_destroy();
+}
 
 }
