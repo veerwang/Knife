02319c254b9f64c526e9df2f356d4e6f88bb7a22
kevin.wang <wwei@landicorp.com>
2017-10-10 15:06:32 +0800

    修正模板问题

--- 11_template/main.cpp
+++ 11_template/main.cpp
@@ -23,6 +23,13 @@
 
 #include "head.h"
 
+class Device
+{
+public:
+	Device(){ std::cout<<"Device create"<<std::endl; }
+	~Device(){ std::cout<<"Device destroy"<<std::endl; }
+};
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
@@ -35,6 +42,14 @@ main ( int argc, char *argv[] )
 	Base<int> base;
 	base.m_ID = 20;
 	std::cout<<base.m_ID<<std::endl;
+
+	BasePolicy<int> bp;
+	bp.Fire();
+
+	Gun<int,MachinePolicy<char>> gun;
+	gun.Trigger();
 	
+	Handle<Device> hd;
+
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
