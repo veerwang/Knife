e364a9bb42eb3f7ef2d906e506feb45152ee9be5
kevin.wang <wwei@landicorp.com>
2017-10-26 17:47:06 +0800

    修正Android.mk 可以编译APOS的程序

--- android-ndk-test/1_hello/jni/Android.mk
+++ android-ndk-test/1_hello/jni/Android.mk
@@ -5,4 +5,7 @@ LOCAL_SRC_FILES := main.cpp
 LOCAL_MODULE := dirtycow
 LOCAL_MODULE_TAGS := optional
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -I$(SYSROOT)/usr/include
+
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 include $(BUILD_EXECUTABLE)
