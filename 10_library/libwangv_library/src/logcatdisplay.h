af5ab1d0ea6903c1805fc077fddb9e67cbe2dc36
kevin.wang <wwei@landicorp.com>
2017-11-01 19:51:19 +0800

    添加logcat的模板版本

---
+++ 10_library/libwangv_library/src/logcatdisplay.h
@@ -0,0 +1,40 @@
+#if !defined(INCLUDED_LOGCATDISPLAY_H)
+#define INCLUDED_LOGCATDISPLAY_H  1
+
+/*
+ * =====================================================================================
+ *
+ *       Filename:  logcatdisplay.h
+ *
+ *    Description:  用于编写日志显示功能
+ *
+ *        Version:  1.0
+ *        Created:  2017年11月01日 19时34分57秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin (WangWei), kevin.wang2004@hotmail.com
+ *        Company:  Landicorp 
+ *   Organization:  GNU
+ *
+ * =====================================================================================
+ */
+
+#include <standard.h>
+#include "wangvlib.h"
+
+namespace WangV
+{
+
+template <typename T>
+class LogcatDisplay 
+{
+public:
+	LogcatDisplay() {;}
+	~LogcatDisplay() {;}
+
+};
+
+}
+
+#endif /* !defined(INCLUDED_LOGCATDISPLAY_H) */
