694e0a2e050a6c022a95d6daffe3099069d0ba73
kevin <kevin.wang2004@hotmail.com>
2017-11-08 22:19:24 +0800

    补充type2type的类型

---
+++ 14_type2type/head.h
@@ -0,0 +1,35 @@
+#if !defined(INCLUDED_HEAD_H)
+#define INCLUDED_HEAD_H  1
+
+/*
+ * =====================================================================================
+ *
+ *       Filename:  head.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/28/2017 11:28:59 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin (WangWei), wwei@landicorp.com
+ *        Company:  Landicorp
+ *   Organization:  GNU
+ *
+ * =====================================================================================
+ */
+
+template <typename T>
+struct type2type
+{
+	typedef T originaltype;
+};
+
+template <typename T,typename U>
+
+
+
+
+
+#endif /* !defined(INCLUDED_HEAD_H) */
