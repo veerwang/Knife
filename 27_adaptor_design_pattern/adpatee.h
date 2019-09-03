409966d9592335d2d3edab4f5f01f392e14e0999
kevin.wang <wwei@landicorp.com>
2019-09-03 15:05:01 +0800

    临时编译适配模式

---
+++ 27_adaptor_design_pattern/adpatee.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDED_ADPATEE_H
+#define INCLUDED_ADPATEE_H
+
+class Adaptee {
+public:
+	Adaptee();
+	Adaptee(const Adaptee&);
+	virtual ~Adaptee();
+	
+protected:
+	
+private:
+	
+};
+
+#endif /* INCLUDED_ADPATEE_H */
