65316dedfee9d9801040fb8fddb14a1466aa729a
kevin.wang <wwei@landicorp.com>
2019-10-28 15:25:01 +0800

    添加get的实验代码

--- 28_unique_ptr/main.cpp
+++ 28_unique_ptr/main.cpp
@@ -21,17 +21,20 @@
 #include <cstdio>
 #include <iostream>
 #include <memory>
+#include <typeinfo>
 
 class Base {
 public:
-	Base() { std::cout << "Base Create" << std::endl; };
+	Base() { m_ID = 0;std::cout << "Base Create" << std::endl; };
 	Base(const Base&);
 	virtual ~Base() { std::cout << "Base Destroy" << std::endl; };
 	
-	void Display() { std::cout << "Base Display" << std::endl; };
+	void Display() { std::cout << "Base Display: " << m_ID << std::endl; };
+	void SetID(int id) { m_ID =  id; };
 protected:
 	
 private:
+	int m_ID;
 	
 };
 
@@ -47,5 +50,26 @@ main ( int argc, char *argv[] )
 	// 自动指针，会自行进行析构
 	std::unique_ptr<Base> ppoint( new Base() );
 	ppoint->Display();
+	ppoint->SetID(20);
+	ppoint->Display();
+
+	// 注意此处 ppoint调用get的用法，是句点的方式不是->方式
+	Base* newpoint = ppoint.get();
+	newpoint->SetID(100);
+	newpoint->Display();
+	ppoint->Display();
+
+	// 注意，此处会将原先的变量进行销毁，在重新初始化
+	ppoint.reset( new Base());
+	// 如果仅仅是ppoint.reset()，那么就是删除之前的变量，并且不进行新变量的创建
+	ppoint->Display();
+
+	Base* happypoint = ppoint.release();
+	// now ppoint是nullpoint
+	happypoint->Display();
+
+	if (ppoint == nullptr)
+		std::cout << "nullptr,can't call it" << std::endl;
+
 	return EXIT_SUCCESS;
 }
