fe171498c343c57ddcffa8f4a185e4666e22856f
kevin.wang <wwei@landicorp.com>
2019-10-31 16:46:26 +0800

    添加预编译头文件

---
+++ 29_precompilehead/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2019年10月31日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "cplusplushead.h"
+
+int main(int argc, const char *argv[])
+{
+	std::cout << "hello the world" << std::endl;
+	return EXIT_SUCCESS;
+}
