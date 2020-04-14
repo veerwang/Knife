3c82b441cc9f95f9b560feb5cbee870c60506f38
kevin.wang <wwei@landicorp.com>
2020-04-14 09:06:45 +0800

    添加事件项目

---
+++ 32-input/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年04月14日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include 	<cstdlib>
+#include 	<cstdio>
+#include 	<iostream>
+
+int main(int argc, const char *argv[])
+{
+	std::cout << "input event programe" << std::endl;
+	return EXIT_SUCCESS;
+}
