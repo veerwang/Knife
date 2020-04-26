822c807f7f82a37cf7e17c23b8339881e27b0aca
kevin <kevin.wang2004@hotmail.com>
2020-04-27 06:21:18 +0800

    添加颜色

---
+++ color/colorsdefine.h
@@ -0,0 +1,172 @@
+#ifndef INCLUDED_COLORSDEFINE_H
+#define INCLUDED_COLORSDEFINE_H
+
+/*
+ * =====================================================================================
+ *
+ *       Filename:  colorsdefine.h 
+ *
+ *    Description:  定义颜色 参考: http://www.wahart.com.hk/rgb.htm
+ *
+ *        Version:  1.0
+ *        Created:  2020年02月04日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  define Color
+ *  Description:  预先定义的颜色 
+ * =====================================================================================
+ */
+namespace WangV
+{
+const RGBA graphic::Yellow(0xff,0xff,0);
+const RGBA graphic::AliceBlue(240, 248, 255);
+const RGBA graphic::AntiqueWhite(250,235,215);
+const RGBA graphic::Aqua(0,255,255);
+const RGBA graphic::Aquamarine(127,255,212);
+const RGBA graphic::Azure(240,255,255);
+const RGBA graphic::Beige(245,245,220);
+const RGBA graphic::Bisque(255,228,196);
+const RGBA graphic::Black(0,0,0);
+const RGBA graphic::BlanchedAlmond(255,235,205);
+const RGBA graphic::Blue(0,0,255);
+const RGBA graphic::BlueViolet(138,43,225);
+const RGBA graphic::Brown(165,42,42);
+const RGBA graphic::BurlyWood(222,184,135);
+const RGBA graphic::CadetBlue(95,158,160);
+const RGBA graphic::Chartreuse(127,255,0);
+const RGBA graphic::Chocolate(210,105,30);
+const RGBA graphic::Colar(255,127,80);
+const RGBA graphic::CornflowerBlue(100,149,237);
+const RGBA graphic::Cornsilk(255,248,220);
+const RGBA graphic::Crimson(220,20,60);
+const RGBA graphic::Cyan(0,255,255);
+const RGBA graphic::DarkBlue(0,0,139);
+const RGBA graphic::DarkCyan(0,139,139);
+const RGBA graphic::DarkGoldenrod(184,134,11);
+const RGBA graphic::DarkGray(169,169,169);
+const RGBA graphic::DarkGreen(0,100,0);
+const RGBA graphic::DarkKhaki(189,183,107);
+const RGBA graphic::DarkMagenta(139,0,139);
+const RGBA graphic::DarkOliveGreen(85,107,47);
+const RGBA graphic::DarkOrange(255,140,0);
+const RGBA graphic::DarkOrchild(153,50,204);
+const RGBA graphic::DarkRed(139,0,0);
+const RGBA graphic::DarkSalmon(233,150,122);
+const RGBA graphic::DarkSeaGreen(143,188,139);
+const RGBA graphic::DarkSlateBlue(72,61,139);
+const RGBA graphic::DarkSlateGray(47,79,79);
+const RGBA graphic::DarkTurquoise(0,206,209);
+const RGBA graphic::DarkViolet(148,0,211);
+const RGBA graphic::DeepPink(255,20,147);
+const RGBA graphic::DeepSkyBlue(0,191,255);
+const RGBA graphic::DimGray(105,105,105);
+const RGBA graphic::DodgetBlue(30,144,255);
+const RGBA graphic::Firebrick(178,34,34);
+const RGBA graphic::FloralWhite(255,250,240);
+const RGBA graphic::ForestGreen(34,139,34);
+const RGBA graphic::Fuchsia(255,0,255,255);
+const RGBA graphic::Gainsboro(220,220,220);
+const RGBA graphic::GhostWhite(248,248,255);
+const RGBA graphic::Gold(255,215,0);
+const RGBA graphic::Goldenrod(218,165,32);
+const RGBA graphic::Gray(128,128,128);
+const RGBA graphic::Green(0,128,0);
+const RGBA graphic::GreenYellow(173,255,47);
+const RGBA graphic::Honeydew(240,255,240);
+const RGBA graphic::HotPink(255,105,180);
+const RGBA graphic::IndianRed(205,92,92);
+const RGBA graphic::Indigo(75,0,130);
+const RGBA graphic::Ivory(255,255,240);
+const RGBA graphic::Khaki(240,230,140);
+const RGBA graphic::Lavender(230,230,250);
+const RGBA graphic::LavenderBlush(255,240,245);
+const RGBA graphic::LawnGreen(124,252,0);
+const RGBA graphic::LemonChiffon(255,250,205);
+const RGBA graphic::LightBlue(173,216,230);
+const RGBA graphic::LightCoral(240,128,128);
+const RGBA graphic::LightCyan(224,255,255);
+const RGBA graphic::LightGoldenrodYellow(250,250,210);
+const RGBA graphic::LightGray(211,211,211);
+const RGBA graphic::LightGreen(144,238,144);
+const RGBA graphic::LightPink(255,182,193);
+const RGBA graphic::LightSalmon(255,160,122);
+const RGBA graphic::LightSeaGreen(32,178,170);
+const RGBA graphic::LightSkyBlue(135,206,250);
+const RGBA graphic::LightSlateGray(119,136,153);
+const RGBA graphic::LightSteelBlue(176,196,222);
+const RGBA graphic::LightYellow(255,255,224);
+const RGBA graphic::Lime(0,255,0);
+const RGBA graphic::LimeGreen(50,205,50);
+const RGBA graphic::Linen(250,240,230);
+const RGBA graphic::Magenta(255,0,255);
+const RGBA graphic::Maroon(128,0,0);
+const RGBA graphic::MediumAquamarine(102,205,170);
+const RGBA graphic::MediumBlue(0,0,205);
+const RGBA graphic::MediumOrchid(186,85,211);
+const RGBA graphic::MediumPurple(147,112,219);
+const RGBA graphic::MediumSeaGreen(60,179,113);
+const RGBA graphic::MediumSlateBlue(123,104,238);
+const RGBA graphic::MediumSpringGreen(0,250,154);
+const RGBA graphic::MediumTurquoise(72,209,204);
+const RGBA graphic::MediumVioletRed(199,21,133);
+const RGBA graphic::MidnightBlue(25,25,112);
+const RGBA graphic::MintCream(245,255,250);
+const RGBA graphic::MistyRose(255,228,225);
+const RGBA graphic::Moccasin(255,228,181);
+const RGBA graphic::NavajoWhite(255,222,173);
+const RGBA graphic::Navy(0,0,128);
+const RGBA graphic::OldLace(253,245,230);
+const RGBA graphic::Olive(128,128,0);
+const RGBA graphic::OliveDrab(107,142,35);
+const RGBA graphic::Orange(255,165,0);
+const RGBA graphic::OrangeRed(255,69,0);
+const RGBA graphic::Orchid(218,112,214);
+const RGBA graphic::PaleGoldenrod(238,232,170);
+const RGBA graphic::PaleGreen(152,251,152);
+const RGBA graphic::PaleTurquoise(175,238,238);
+const RGBA graphic::PaleVioletRed(219,112,147);
+const RGBA graphic::PapayaWhip(255,239,213);
+const RGBA graphic::PeachPuff(255,218,185);
+const RGBA graphic::Peru(205,133,63);
+const RGBA graphic::Pink(255,192,203);
+const RGBA graphic::Plum(221,160,221);
+const RGBA graphic::PowderBlue(176,224,230);
+const RGBA graphic::Purple(128,0,128);
+const RGBA graphic::Red(255,0,0);
+const RGBA graphic::RosyBrown(188,143,143);
+const RGBA graphic::RoyalBlue(65,105,225);
+const RGBA graphic::SaddleBrown(139,69,19);
+const RGBA graphic::Salmon(250,128,114);
+const RGBA graphic::SandyBrown(255,164,96);
+const RGBA graphic::SeaGreen(46,139,87);
+const RGBA graphic::SeaShell(255,245,238);
+const RGBA graphic::Sienna(160,82,45);
+const RGBA graphic::Silver(192,192,192);
+const RGBA graphic::SkyBlue(135,206,235);
+const RGBA graphic::SlateBlue(106,90,205);
+const RGBA graphic::SlateGray(112,128,144);
+const RGBA graphic::Snow(255,250,250);
+const RGBA graphic::SpringGreen(0,255,127);
+const RGBA graphic::SteelBlue(70,130,180);
+const RGBA graphic::Tan(210,180,140);
+const RGBA graphic::Teal(0,128,128);
+const RGBA graphic::Thistle(216,191,216);
+const RGBA graphic::Tomato(255,99,71);
+const RGBA graphic::Transparant(0,0,0,0);
+const RGBA graphic::Turqouise(65,224,208);
+const RGBA graphic::Violet(238,130,238);
+const RGBA graphic::Wheat(245,222,179);
+const RGBA graphic::White(255,255,255);
+const RGBA graphic::WhiteSmoke(245,245,245);
+const RGBA graphic::YellowGreen(154,205,50);
+}
+#endif /* INCLUDED_COLORSDEFINE_H */
