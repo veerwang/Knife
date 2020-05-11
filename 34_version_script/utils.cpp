aab58bbea8cdca374596678ad78c4daa67d774a0
kevin.wang <wwei@landicorp.com>
2020-05-11 11:00:58 +0800

    临时添加version_script的测试

---
+++ 34_version_script/utils.cpp
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年05月11日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include 	<cstdlib>
+#include 	<cstdio>
+#include 	<iostream>
+
+/* version V1.0.0 */
+void display_version() {
+	std::cout << "This version V1.0.0" << std::endl;
+}
+
+void do_action() {
+	std::cout << "Do action start" << std::endl;
+}
