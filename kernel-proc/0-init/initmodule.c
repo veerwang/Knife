23a7cf2f411139dbee02065f8f5526a24338727a
kevin.wang <wwei@landicorp.com>
2020-06-19 15:18:56 +0800

    修正驱动

--- kernel-proc/0-init/initmodule.c
+++ kernel-proc/0-init/initmodule.c
@@ -2,11 +2,26 @@
 #include<linux/kernel.h>  //内核头文件
 #include<linux/module.h>  //模块的头文件
  
+/*
+#define KERN_EMERG     "<0>"
+#define KERN_ALERT     "<1>"
+#define KERN_CRIT      "<2>"
+#define KERN_ERR       "<3>"
+#define KERN_WARNING   "<4>"
+#define KERN_NOTICE    "<5>"
+#define KERN_INFO      "<6>"
+#define KERN_DEBUG     "<7>"
+级别越小，越紧急
+*/
+
 MODULE_LICENSE("shaohef  BSD/GPL");
  
 static int __init initmodule_start(void)
 {
+    printk(KERN_EMERG "Loading initmodule...\n");
     printk(KERN_ALERT "Loading initmodule...\n");
+    printk(KERN_ERR   "Loading initmodule...\n");
+    printk(KERN_DEBUG "Loading initmodule...\n");
     return 0;
 }
  
