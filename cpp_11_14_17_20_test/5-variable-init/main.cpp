545bd39496f00f20108ce4e34d820cbed4ee61c4
kevin.wang <wwei@landicorp.com>
2020-06-28 10:45:15 +0800

    添加初始化代码

---
+++ cpp_11_14_17_20_test/5-variable-init/main.cpp
@@ -0,0 +1,49 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年06月28日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include 	<cstdlib>
+#include 	<cstdio>
+#include 	<iostream>
+
+class object {
+public:
+	/* 此处的初始化滞后于{}初始化，因此最终的数值是20 */
+	object () : id(20) {;}
+	object (const object&) {;}
+	virtual ~object() {;}
+
+	void display() {
+		std::cout << "id = " << id << std::endl;
+	}
+
+protected:
+	
+private:
+	/* 初始化 */
+	int id = {10};
+};
+
+
+
+int main(int argc, const char *argv[]) {
+	std::cout << "variables init" << std::endl;	
+	object obj;
+	obj.display();
+
+	return EXIT_SUCCESS;
+}
