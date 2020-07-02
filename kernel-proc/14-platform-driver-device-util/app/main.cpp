90de215b008b7d639d13d599ca2fb80a98c8aa0b
kevin.wang <wwei@landicorp.com>
2020-07-02 13:30:46 +0800

    添加完整的基于platfrom-driver-device模型的范例代码

---
+++ kernel-proc/14-platform-driver-device-util/app/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年07月02日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+
+#include <sys/mman.h> 
+#include <sys/ioctl.h> 
+#include <fcntl.h> 
+#include <unistd.h>
+
+int main(int argc, const char *argv[]) {
+	std::cout << "driver test" << std::endl;	
+	FILE* fp = NULL;
+	if ( (fp = fopen("/dev/eddydriver","a+")) == NULL ) {
+		perror("open /dev/eddy0 fail");
+		return EXIT_SUCCESS;
+	}
+
+	fclose(fp);
+	return EXIT_SUCCESS;
+}
