ec5ce12f32b2b1d428fa24fb681cb0825b5ab485
kevin.wang <wwei@landicorp.com>
2020-07-07 13:24:05 +0800

    添加array

---
+++ cpp_11_14_17_20_test/9-array/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年06月24日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include 	<cstdlib>
+#include 	<cstdio>
+#include 	<iostream>
+
+/* 固定大小的数组 */
+#include	<array>
+
+int main(int argc, const char *argv[]) {
+	std::cout << "array test V1.0.0" << std::endl;
+	constexpr int maxlength = 10;
+	std::array<int,maxlength> buf = {1,2,3,4,5,6,7,8,9,0};
+	for (e : buf) {
+		std::cout << e << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
