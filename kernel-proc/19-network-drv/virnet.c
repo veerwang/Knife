227d8709f1338d34322227edc12953d3025656ee
kevin.wang <wwei@landicorp.com>
2020-07-09 10:16:38 +0800

    添加能够接收数据的网络驱动

--- kernel-proc/19-network-drv/virnet.c
+++ kernel-proc/19-network-drv/virnet.c
@@ -11,12 +11,26 @@ static struct net_device *sg_dev;
 // 有数据帧要发送时，kernel会调用该函数
 static int virnet_send_packet(struct sk_buff *skb,struct net_device *dev)
 {
+    int i;
+    // 告诉内核不要传入更多的帧
+    netif_stop_queue(dev);
+    
+    // 打印出数据帧
+    printk("send, length=%d:\n",skb->len);
+    for(i=0;i<skb->len;i++)
+        printk("%x ",skb->data[i]);
+    printk("\n");
+	
     // 统计已发送的数据包
     dev->stats.tx_packets++;
     // 统计已发送的字节
     dev->stats.tx_bytes+=skb->len;
+    
     // 释放数据帧
     dev_kfree_skb(skb);
+    
+    // 告诉内核可以传入更多帧了
+    netif_wake_queue(dev);
     return 0;
 }
 
@@ -33,11 +47,11 @@ static int virnet_init(void)
     // 创建一个网络设备，名为“virnet%d"，kernel会自动填写%d为网卡编号
     //sg_dev=alloc_netdev(0,"virnet%d",ether_setup);
     // kernel 4.4.0-97上需要四个参数如下
-    sg_dev=alloc_netdev(0,"virnet%d", NET_NAME_UNKNOWN, ether_setup);
+    sg_dev=alloc_netdev(0,"kevinnet%d", NET_NAME_UNKNOWN, ether_setup);
     // 该网络设备的操作集
     sg_dev->netdev_ops=&sg_ops;
     // MAC地址是01:02:03:04:05:06
-    memcpy(sg_dev->dev_addr,"\x01\x02\x03\x04\x05\x06",6);
+    memcpy(sg_dev->dev_addr,"\x01\x02\x03\x04\x05\x09",6);
     // 注册网络设备
     register_netdev(sg_dev);
     return 0;
