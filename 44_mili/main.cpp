1000568a14d107bfbdad6066f26d16abbf7f39d2
kevin.wang <wwei@landicorp.com>
2020-07-14 15:15:38 +0800

    添加mili测试代码

--- 44_mili/main.cpp
+++ 44_mili/main.cpp
@@ -1,69 +1,116 @@
 /*
- * =====================================================================================
- *
- *       Filename:  main.cpp
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  2020年06月16日 
- *       Revision:  1 
- *       Compiler:  gcc
- *
- *         Author:  kevin.wang 
- *   Organization:  
- *
- * =====================================================================================
- */
+platform-detection:
+    This file is part of the MiLi Minimalistic Library.
 
-#include 	<cstdlib>
-#include 	<cstdio>
-#include 	<iostream>
-#include	"mili/mili.h"
+    Copyright (C) Daniel Muñoz, FuDePAN 2012
+    Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE_1_0.txt in the root directory or 
+    copy at http://www.boost.org/LICENSE_1_0.txt)
+    
+    MiLi IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+    SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+    FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
 
-using namespace mili;
+    This is an example file.
+*/
 
-int main(int argc, const char *argv[])
+#include <iostream>
+#include "mili/mili.h"
+
+/* Operating System example */
+#if defined (MILI_OS)
+#    if   MILI_OS == MILI_OS_LINUX
+#        define OS_STRING "Linux"
+#    elif MILI_OS == MILI_OS_WINDOWS
+#        define OS_STRING "Windows"
+#    elif MILI_OS == MILI_OS_MAC
+#        define OS_STRING "Mac"
+#    elif MILI_OS == MILI_OS_UNKNOWN
+#        define OS_STRING "Unknown"
+#    else
+#        define OS_STRING "Error (MILI_OS has an invalid value)"
+#    endif
+
+#else /* !defined (MILI_OS) */
+#    define OS_STRING "Error (MILI_OS is not defined)"
+
+#endif /* defined (MILI_OS) */
+
+/* Compiler example */
+#if defined (MILI_COMPILER)
+#    if   MILI_COMPILER == MILI_COMPILER_VS
+#        define COMPILER_STRING "Visual C++"
+#    elif MILI_COMPILER == MILI_COMPILER_GCC
+#        define COMPILER_STRING "GNU-GCC"
+#    elif MILI_COMPILER == MILI_COMPILER_ICC
+#        define COMPILER_STRING "Intel C/C++"
+#    elif MILI_COMPILER == MILI_COMPILER_UNKNOWN
+#        define COMPILER_STRING "Unknown"
+#    else
+#        define COMPILER_STRING "Error (MILI_COMPILER has an invalid value)"
+#    endif
+
+#else /* !defined (MILI_COMPILER) */
+#    define COMPILER_STRING "Error (MILI_COMPILER is not defined)"
+
+#endif /* defined (MILI_COMPILER) */
+
+int main()
 {
-	std::cout << "mili test" << std::endl;	
-    	std::cout << mili::getGPL3LicenseText("mili", "16", "Fudepan Team", "2012");
+    int result = EXIT_SUCCESS;
 
-	std::vector<int> v(5, 3); //all 3's
-	v[1] = 1;
-	v[4] = 7; //so it is [3,1,3,3,7]
+    std::cout<<"switch/case output: ";
 
-	bostream<> bos;
-	bos << 1 << 2 << 3 << std::string("Hello ") << v << 4 << std::string("World!");
+    /* Another Operating System example */
+    switch(MILI_OS)
+    {
+        case MILI_OS_LINUX:
+            std::cout<<"Linux"; 
+            break;
+        case MILI_OS_WINDOWS:
+            std::cout<<"Windows";
+            break;
+        case MILI_OS_MAC:
+            std::cout<<"Mac";
+            break;
+        case MILI_OS_UNKNOWN:
+            std::cout<<"Unknown";
+            break;
+        default:
+            std::cout<<"Error (MILI_OS has an invalid value)";
+            result = EXIT_FAILURE;
+    }
 
-	bostream<> bos2;
-	bos2 << 100;
-	bos +=  bos2;
+    std::cout<<" OS / ";
 
-	bistream<> bis(bos.str());
+    /* Another Compiler example */
+    switch(MILI_COMPILER)
+    {
+        case MILI_COMPILER_VS:
+            std::cout<<"Visual C++"; 
+            break;
+        case MILI_COMPILER_GCC:
+            std::cout<<"GNU-GCC";
+            break;
+        case MILI_COMPILER_ICC:
+            std::cout<<"Intel C/C++";
+            break;
+        case MILI_COMPILER_UNKNOWN:
+            std::cout<<"Unknown";
+            break;
+        default:
+            std::cout<<"Error (MILI_COMPILER has an invalid value)";
+            result = EXIT_FAILURE;
+    }
 
-	int         nums[4];
-	std::string str1;
-	std::string str2;
+    std::cout<<" Compiler"<<std::endl;
+    
+    std::cout<<"Conditional compilation output: " OS_STRING " OS / " COMPILER_STRING " Compiler"<<std::endl;
 
-	std::vector<int> v2;
-
-	bis >> nums[0] >> nums[1] >> nums[2] >> str1 >> v2 >> nums[3] >> str2;
-
-	for (int i = 0; i < 4 ; ++i)
-		std::cout << nums[i] << std::endl;
-
-	std::cout << str1 << str2 << std::endl;
-
-	std::cout << '[';
-	for (size_t i = 0; i < 5; ++i)
-		std::cout << v2[i] << ' ';
-	std::cout << ']' << std::endl;
-
-	bos.clear();
-
-	container_writer<int> cw(5, bos);
-
-	cw << 1 << 2 << 3 << 4 << 5;
-
-	return EXIT_SUCCESS;
+    return result;
 }
+
