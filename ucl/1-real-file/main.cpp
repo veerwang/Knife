b1e47a55e1c7db3f2333dac21df08382aa20ee75
kevin.wang <wwei@landicorp.com>
2020-08-25 17:12:20 +0800

    修正压缩范例

--- ucl/1-real-file/main.cpp
+++ ucl/1-real-file/main.cpp
@@ -16,12 +16,15 @@
  * =====================================================================================
  */
 
-#include 	<cstdlib>
-#include 	<cstdio>
-#include 	<iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <vector>
 
-#include 	<ucl/ucl.h>
-#include	"uclmrg.h"
+#include <ucl/ucl.h>
+
+#include "uclmrg.h"
+#include "fileopt.h"
 
 int 
 main(int argc, const char *argv[]) {
@@ -32,13 +35,26 @@ main(int argc, const char *argv[]) {
 		std::cout << "uclmrg init sccessful" << std::endl;
 	}
 
+	std::vector<char> context;
+	ZFile zf;
+	zf.Read("README.md",context);
+	char *buf = new char[context.size()];
+	int l = context.size();
+	for ( int i=0;i<l;i++ ) {
+		buf[i] = context[i];
+	}
+	uclm.config((u_int8_t *)buf,l);
+	uclm.compress();
+
+	u_int8_t* out = uclm.outbuf;
+	u_int32_t ol  = uclm.out_length;
+	zf.Write("README.md.ucl",(const void *)out,ol);
 
 
 
 
-
-
-
+	delete[] buf;
+	buf = nullptr;
 	uclm.release();
 
 	return EXIT_SUCCESS;
