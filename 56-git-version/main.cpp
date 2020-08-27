f7fe43d54de118c4309c37916d2a7e5c8a52936c
kevin.wang <wwei@landicorp.com>
2020-08-27 14:30:24 +0800

    添加git与版本关联代码

---
+++ 56-git-version/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年08月27日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include 	<cstdlib>
+#include 	<cstdio>
+#include 	<iostream>
+
+int 
+main(int argc, const char *argv[]) {
+	std::cout << "Version: " << VERSION << std::endl;	
+	return EXIT_SUCCESS;
+}
