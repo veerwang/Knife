4d0a6b9aefa0973c6bdab47e569b0b4d77707017
kevin.wang <wwei@landicorp.com>
2020-09-21 16:52:58 +0800

    修正类型

--- 61-class-enum/main.cpp
+++ 61-class-enum/main.cpp
@@ -20,15 +20,25 @@
 #include 	<cstdio>
 #include 	<iostream>
 
+/* C++11 比较安全的形式，可防止不同的类型之间直接进行比较 */
 enum class WEEK : uint32_t {
 	MONDAY  = 1,
 	FRIDAY  = 2,
 	SUNDAY  = 7,
+	ENDDAY  = 0xfffff,
+};
+
+enum class HUMAN : bool {
+	MAN	= true,
+	WOMAN	= false,
 };
 
 int
 main(int argc, const char *argv[]) {
 	std::cout << "class enum test" << std::endl;
 	printf("%d\n",static_cast<int>(WEEK::FRIDAY));
+	printf("%x\n",static_cast<int>(WEEK::ENDDAY));
+	if ( HUMAN::MAN == (HUMAN)WEEK::MONDAY )
+		std::cout << "GOOD" << std::endl;
 	return EXIT_SUCCESS;
 }
