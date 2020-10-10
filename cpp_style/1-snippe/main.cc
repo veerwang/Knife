fe89f8efef7816a818287fba44677151d919d06a
kevin.wang <wwei@landicorp.com>
2020-10-10 15:32:22 +0800

    添加snippe实验代码

---
+++ cpp_style/1-snippe/main.cc
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  初始化程序的入口
+ *
+ *        Version:  1.0
+ *        Created:  2020年10月10日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include 	<cstdlib>
+#include 	<cstdio>
+#include 	<iostream>
+
+#include 	"base.h"
+
+int 
+main(int argc, const char *argv[]) {
+	
+	return EXIT_SUCCESS;
+}
