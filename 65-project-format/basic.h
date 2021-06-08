c419e9b778264665eecd173ebabb293a5a74142a
kevin.wang <kevin@wisbo.com>
2021-06-08 13:47:51 +0800

    修正格式

---
+++ 65-project-format/basic.h
@@ -0,0 +1,31 @@
+#ifndef INCLUDED_BASIC_H
+#define INCLUDED_BASIC_H
+
+/*
+ * =============================================================================
+ *
+ *       Filename:  basic.h
+ *
+ *    Description:  基本框架类
+ *
+ *        Version:  1.0
+ *        Created:  2021年06月08日
+ *       Revision:  1
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang
+ *   Organization:
+ *
+ * =============================================================================
+ */
+class basic {
+  public:
+        basic();
+        basic(const basic&);
+        virtual ~basic();
+
+  protected:
+  private:
+};
+
+#endif /* INCLUDED_BASIC_H */
---
+++ 65-project-format/main.cc
@@ -0,0 +1,53 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  测试使用
+ *
+ *        Version:  1.0
+ *        Created:  2021年06月07日
+ *       Revision:  1
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang
+ *   Organization:
+ *
+ * =============================================================================
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#include "basic.h"
+
+void print_hello(int times) {
+        for (int i = 0; i < times; i++) {
+                std::cout << "hello the word" << std::endl;
+        }
+}
+
+/*
+ * ===  FUNCTION  =============================================================
+ *         Name:  main
+ *  Description:  程序的入口
+ * ============================================================================
+ */
+int main(int argc, const char *argv[]) {
+        std::cout << "hello" << std::endl;
+        print_hello(2);
+        if (true) {
+                std::cout << "hello" << std::endl;
+        }
+        int num = 0;
+        switch (num) {
+                case 0:
+                        std::cout << "hello" << std::endl;
+                        break;
+                default:
+			std::cout << "baby" << std::endl;
+        }
+        basic b;
+        return EXIT_SUCCESS;
+}
