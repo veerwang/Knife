4e36e1273e81cb365ae6b2580937cd82e005f78a
kevin.wang <kevin@wisbo.com>
2021-08-05 17:04:35 +0800

    修正滑块功能

--- agg-programe/100-slider/main.cc
+++ agg-programe/100-slider/main.cc
@@ -64,13 +64,14 @@ bool write_ppm(const unsigned char *buf, unsigned width, unsigned height,
         return false;
 }
 
-/*
- * ===  FUNCTION
- * ====================================================================== Name:
- *        Input:  draw_main_sharp
+/* 
+ * ===  FUNCTION  =============================================================
+ *         Name:  draw_main_sharp
+ *        Input:  left,right,up,down: 滑块边框的坐标 
+ *        	  radium: 半径
  *       Output:
- *  Description:  绘制圆角矩形
- * =====================================================================================
+ *  Description:  绘制圆角矩形,同时也是滑块的主体
+ * ============================================================================
  */
 void draw_main_sharp(agg::rendering_buffer &rbuf, int left, int up, int right,
                      int down, int radium) {
@@ -103,24 +104,24 @@ void draw_main_sharp(agg::rendering_buffer &rbuf, int left, int up, int right,
         agg::render_scanlines(ras, sl, rens);
 }
 
-/*
- * ===  FUNCTION
- * ====================================================================== Name:
- *        Input:  draw_mid_line
+/* 
+ * ===  FUNCTION  =============================================================
+ *         Name:  draw_mid_line
+ *        Input:  left,up,right,down:整个滑块控件外框的坐标 
+ *        	  xdis,中线偏离外框左边缘的距离
  *       Output:
- *  Description:  绘制中线
- * =====================================================================================
+ *  Description:  绘制中线   
+ * ============================================================================
  */
 void draw_mid_line(agg::rendering_buffer &rbuf, int left, int up, int right,
-                   int down, agg::rgba8 color) {
+                   int down, int xdis ,agg::rgba8 color) {
         typedef agg::pixfmt_rgb24 pixfmt_type;
         typedef agg::renderer_base<agg::pixfmt_rgb24> renbase_type;
         typedef agg::renderer_scanline_aa_solid<renbase_type>
             render_scanline_type;
 
-        int xdis = 10;
         int midlinex = left + xdis;
-        int midliney = up + ((down - up) >> 1);
+        int midliney = up + ((down - up) >> 1) + 1;
 
         pixfmt_type pixf(rbuf);
         // 关键代码
@@ -150,13 +151,16 @@ void draw_mid_line(agg::rendering_buffer &rbuf, int left, int up, int right,
         agg::render_scanlines(ras, sl, rens);
 }
 
-/*
- * ===  FUNCTION
- * ====================================================================== Name:
- *        Input:  draw_circle
+/* 
+ * ===  FUNCTION  =============================================================
+ *         Name:  draw_circle
+ *        Input:  up,down: 滑块外框的上下坐标
+ *        	  radium: 滑块半径
+ *        	  indicate: 滑块x坐标
+ *        	  radium: 小滑块的半径 
  *       Output:
- *  Description:  绘制圆块
- * =====================================================================================
+ *  Description:  绘制圆块   
+ * ============================================================================
  */
 void draw_circle(agg::rendering_buffer &rbuf, int up, int down, int indicate,
                  int radium, agg::rgba8 color, bool shadowflag) {
@@ -202,25 +206,28 @@ void draw_circle(agg::rendering_buffer &rbuf, int up, int down, int indicate,
         }
 }
 
-/*
- * ===  FUNCTION
- * ====================================================================== Name:
- *        Input:  draw_slider
+/* 
+ * ===  FUNCTION  =============================================================
+ *         Name:  draw_slider
+ *        Input:  left,up,right,down:滑块外框的坐标 
+ *        	  radium: 滑块的半径
+ *        	  nodepos: 滑块相对外框最左边的距离
  *       Output:
- *  Description:  绘制圆角矩形
- * =====================================================================================
+ *  Description:  绘制滑块功能 
+ * ============================================================================
  */
 void draw_slider(agg::rendering_buffer &rbuf, int left, int up, int right,
-                 int down, int radium) {
+                 int down, int radium, int nodepos) {
+	// 12 这个魔数是中条距离滑块外框的间距 
+	// 8 这个数字是小滑块的半径
         draw_main_sharp(rbuf, left, up, right, down, radium);
-        draw_mid_line(rbuf, left, up, right, down,
+        draw_mid_line(rbuf, left, up, right, down, 12,
                       agg::rgba8(0xA9, 0xA9, 0xA9, 0xff));
-        int indicate = right - 50;
-        draw_mid_line(rbuf, left, up, indicate, down,
+        draw_mid_line(rbuf, left + 12, up, left + nodepos, down, 0,
                       agg::rgba8(0x00, 0x00, 0xFF, 0xff));
-        draw_circle(rbuf, up, down, indicate, 8,
+        draw_circle(rbuf, up, down, left + nodepos, 8,
                     agg::rgba8(0x00, 0x00, 0x00, 0xFF), true);
-        draw_circle(rbuf, up, down, indicate, 8,
+        draw_circle(rbuf, up, down, left + nodepos, 8,
                     agg::rgba8(0xFF, 0xFF, 0xFF, 0xFF), false);
 }
 
@@ -228,7 +235,7 @@ int main(int argc, const char *argv[]) {
         std::cout << "agg library program" << std::endl;
 
         int frame_width = 320;
-        int frame_height = 240;
+        int frame_height = 480;
 
         typedef agg::pixfmt_rgb24 pixfmt_type;
         typedef agg::renderer_base<agg::pixfmt_rgb24> renbase_type;
@@ -254,8 +261,12 @@ int main(int argc, const char *argv[]) {
         // 背景颜色设置
 
         // 绘制图形
-        draw_slider(rbuf, 50, 50, 280, 90, 10);
+        draw_slider(rbuf, 50, 50, 280, 90, 10, 10);
+        draw_slider(rbuf, 50, 50 + 50, 280, 90 + 50, 10, 280 - 50 + 1 - 10);
+        draw_slider(rbuf, 50, 50 + 100, 280, 90 + 100, 10, 280 - 50 + 1 - 100);
 
+
+	//显示到图片
         write_ppm(buffer, frame_width, frame_height, "agg_test.ppm");
         delete[] buffer;
         buffer = NULL;
