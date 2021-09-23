b6824d755f8e11b16927bbee1b94b8e4793764a4
kevin <kevin.wang2004@hotmail.com>
2021-09-23 14:35:47 +0800

    添加using初始化变量

---
+++ 67-using-init/device.h
@@ -0,0 +1,36 @@
+#ifndef INCLUDED_DEVICE_H
+#define INCLUDED_DEVICE_H
+
+/*
+ * =============================================================================
+ *
+ *       Filename:  device.h
+ *
+ *    Description:  类介绍 
+ *
+ *        Version:  1.0
+ *        Created:  2021年09月23日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+
+#include "base.h"
+
+class Device : public Base {
+  public:
+	Device ();
+	Device (const Device&);
+	virtual ~Device();
+
+  protected:
+	
+  private:
+	
+};
+
+#endif /* INCLUDED_DEVICE_H */
---
+++ 67-using-init/main.cc
@@ -0,0 +1,29 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  测试C++11的using构造函数 
+ *
+ *        Version:  1.0
+ *        Created:  2021年09月23日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+
+#include "device.h"
+
+int
+main(int argc, const char *argv[]) {
+	Device b;	
+	return EXIT_SUCCESS;
+}
