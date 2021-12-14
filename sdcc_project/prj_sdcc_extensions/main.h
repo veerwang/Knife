df34ffe3a50960246552fd42fb28c8a7691635cf
kevin.wang <kevin@wisbo.com>
2021-12-14 17:24:48 +0800

    [8051] 添加51语言的特性

---
+++ sdcc_project/prj_sdcc_extensions/main.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDED_MAIN_H
+#define INCLUDED_MAIN_H
+
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description:  工程头文件 
+ *
+ *        Version:  1.0
+ *        Created:  2021年12月13日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+
+extern int init_frame();
+extern int device_init();
+
+#endif /* INCLUDED_MAIN_H */
---
+++ sdcc_project/prj_sdcc_extensions/utils.c
@@ -0,0 +1,6 @@
+#include <mcs51/C8051F920.h>
+
+int init_frame()
+{
+	return 0;
+}
