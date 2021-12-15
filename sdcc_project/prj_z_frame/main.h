6e9318934aa16fc92b059cfe0e977480bd0316e2
kevin.wang <kevin@wisbo.com>
2021-12-15 09:18:24 +0800

    [8051] 添加z工程框架

---
+++ sdcc_project/prj_z_frame/main.h
@@ -0,0 +1,27 @@
+#ifndef INCLUDED_MAIN_H
+#define INCLUDED_MAIN_H
+
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description:  工程头文件 
+ *
+ *        Version:  1.0
+ *        Created:  2021年12月13日 
+ *       Revision:  1 
+ *       Compiler:  gcc
+ *
+ *         Author:  kevin.wang 
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+
+/* utils.c */
+extern int init_frame();
+
+extern int device_init();
+
+#endif /* INCLUDED_MAIN_H */
