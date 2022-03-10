39073fdafb30887e19f45aa95f97a83945771cbb
kevin.wang <kevin@wisbo.com>
2022-03-10 17:32:11 +0800

    修正代码

--- kernel-proc/22-poll/initmodule.c
+++ kernel-proc/22-poll/initmodule.c
@@ -1,168 +1,201 @@
-#include<linux/slab.h>		//kmalloc 函数定义
-#include<linux/init.h>  	//初始换函数
-#include <linux/cdev.h>
-#include <linux/fs.h>
-#include<linux/kernel.h>  	//内核头文件
-#include<linux/module.h>  	//模块的头文件
-#include<linux/device.h>	//class_create/device_create相关头文件的定义
-				//相关头文件在KERNELDIR中有定义
-
-#include <linux/kdev_t.h>
+/***************************************************************************//**
+*  \file       driver.c
+*
+*  \details    Simple Linux device driver (sysfs)
+*
+*  \author     EmbeTronicX
+*
+*  \Tested with Linux raspberrypi 5.10.27-v7l-embetronicx-custom+
+*
+*******************************************************************************/
 #include <linux/kernel.h>
-#include <linux/kobject.h>
-#include <linux/kthread.h>
-#include <linux/wait.h>	   	//Required for the wait queues
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include<linux/slab.h>                 //kmalloc()
+#include<linux/uaccess.h>              //copy_to/from_user()
+#include<linux/sysfs.h> 
+#include<linux/kobject.h> 
+ 
+ 
+volatile int etx_value = 0;
+ 
+ 
+dev_t dev = 0;
+static struct class *dev_class;
+static struct cdev etx_cdev;
+struct kobject *kobj_ref;
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("kevin.wang");
-
-//Waitqueue
-DECLARE_WAIT_QUEUE_HEAD(wait_queue_etx_data);
-
-static dev_t devId;
-static struct class *cls = NULL;
-struct cdev cdev;
-
-static char etx_value[20];
-
-struct kobject* kobj_ref;
-
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
-
-/*************** Sysfs Fuctions **********************/
-static ssize_t sysfs_show(struct kobject* kobj, struct kobj_attribute* attr, char* buf);
-static ssize_t sysfs_store(struct kobject* kobj, struct kobj_attribute* attr, const char* buf, size_t count);
+/*
+** Function Prototypes
+*/
+static int      __init etx_driver_init(void);
+static void     __exit etx_driver_exit(void);
+ 
+/*************** Driver functions **********************/
+static int      etx_open(struct inode *inode, struct file *file);
+static int      etx_release(struct inode *inode, struct file *file);
+static ssize_t  etx_read(struct file *filp, 
+                        char __user *buf, size_t len,loff_t * off);
+static ssize_t  etx_write(struct file *filp, 
+                        const char *buf, size_t len, loff_t * off);
+ 
+/*************** Sysfs functions **********************/
+static ssize_t  sysfs_show(struct kobject *kobj, 
+                        struct kobj_attribute *attr, char *buf);
+static ssize_t  sysfs_store(struct kobject *kobj, 
+                        struct kobj_attribute *attr,const char *buf, size_t count);
 
 struct kobj_attribute etx_attr = __ATTR(etx_value, 0660, sysfs_show, sysfs_store);
 
-struct file_operations eddy_fops = {
-       .owner 	 = THIS_MODULE,
-       .open  	 = device_open,
-       .release  = device_release,
-       .read     = device_read,
-       .write    = device_write,
+/*
+** File operation sturcture
+*/
+static struct file_operations fops =
+{
+        .owner          = THIS_MODULE,
+        .read           = etx_read,
+        .write          = etx_write,
+        .open           = etx_open,
+        .release        = etx_release,
 };
 
 /*
 ** This function will be called when we read the sysfs file
 */
-static ssize_t sysfs_show(struct kobject* kobj,
-    struct kobj_attribute* attr,
-    char* buf)
+static ssize_t sysfs_show(struct kobject *kobj, 
+                struct kobj_attribute *attr, char *buf)
 {
-	pr_info("Sysfs Show - Write Permission Granted!!!\n");
-
-	//wake up the waitqueue
-	wake_up(&wait_queue_etx_data);
-
-	return sprintf(buf, "%s", "Success\n");
+        pr_info("Sysfs - Read!!!\n");
+        return sprintf(buf, "%d", etx_value);
 }
 
 /*
-** This function will be called when we write the sysfs file
+** This function will be called when we write the sysfsfs file
 */
-static ssize_t sysfs_store(struct kobject* kobj,
-    struct kobj_attribute* attr,
-    const char* buf,
-    size_t count)
+static ssize_t sysfs_store(struct kobject *kobj, 
+                struct kobj_attribute *attr,const char *buf, size_t count)
 {
-	pr_info("Sysfs Store - Read Permission Granted!!!\n");
-
-	strcpy(etx_value, buf);
-
-	//wake up the waitqueue
-	wake_up(&wait_queue_etx_data);
-
-	return count;
+        pr_info("Sysfs - Write!!!\n");
+        sscanf(buf,"%d",&etx_value);
+        return count;
 }
 
-static ssize_t device_write(struct file *mfile, const char __user *databuf, size_t mlen, loff_t *mloff) {
-	return 0; 
+/*
+** This function will be called when we open the Device file
+*/ 
+static int etx_open(struct inode *inode, struct file *file)
+{
+        pr_info("Device File Opened...!!!\n");
+        return 0;
 }
 
-static ssize_t device_read(struct file *mfile, char __user *databuf, size_t mlen, loff_t *mloff) {
-	return 0; 
-}
-
-static int device_open(struct inode * mindoe, struct file *mfile) {
-	return 0;
-}
-
-static int device_release(struct inode *minode, struct file *mfile) {
-	return 0;
+/*
+** This function will be called when we close the Device file
+*/ 
+static int etx_release(struct inode *inode, struct file *file)
+{
+        pr_info("Device File Closed...!!!\n");
+        return 0;
 }
  
-static int __init initmodule_start(void)
+/*
+** This function will be called when we read the Device file
+*/
+static ssize_t etx_read(struct file *filp, 
+                char __user *buf, size_t len, loff_t *off)
 {
-	int result;
-
-	printk(KERN_ALERT "Loading initmodule...\n");
-
-	// 动态申请主设备号
-	//if(( result = alloc_chrdev_region(&devId, 0, 1, "stone-alloc-dev") ) != 0) {
-	if(( result = alloc_chrdev_region(&devId, 0, 1, "eddy-dev") ) != 0) {
-		printk(KERN_WARNING "register dev id error:%d\n", result);
-		goto err;
-	} else {
-		printk(KERN_WARNING "register dev id success!\n");
-	}
-
-	cls = class_create(THIS_MODULE, "eddy-class");
-        if(IS_ERR(cls)) {
-                printk(KERN_WARNING "create class error!\n");
-                goto err;
-        }
-
-	// 设置设备节点的文件操作函数
-	cdev_init (&cdev, &eddy_fops);
-	cdev.owner = THIS_MODULE;
-	cdev.ops = &eddy_fops;
-	result = cdev_add (&cdev, devId , 1);
-	if (result)
-		printk (KERN_WARNING "Error %d adding char_reg_setup_cdev", result);
-
-	//实际在/dev目录下创建eddy%d的设备节点
-        if(device_create(cls, NULL, devId, "", "eddy%d", 0) == NULL) {
-                printk(KERN_WARNING "create device error!\n");
-                goto err;
-        }
-
-	/*Creating a directory in /sys/kernel/ */
-	kobj_ref = kobject_create_and_add("etx_sysfs", kernel_kobj);
-
-	/*Creating sysfs file for etx_value*/
-	if (sysfs_create_file(kobj_ref, &etx_attr.attr)) {
-		printk(KERN_INFO "Cannot create sysfs file......\n");
-		goto r_sysfs;
-	}
-
-        printk(KERN_ALERT "initmodule load success!\n");
+        pr_info("Read function\n");
         return 0;
+}
 
+/*
+** This function will be called when we write the Device file
+*/
+static ssize_t etx_write(struct file *filp, 
+                const char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Write Function\n");
+        return len;
+}
+ 
+/*
+** Module Init function
+*/
+static int __init etx_driver_init(void)
+{
+        /*Allocating Major number*/
+        if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
+                pr_info("Cannot allocate major number\n");
+                return -1;
+        }
+        pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+ 
+        /*Creating cdev structure*/
+        cdev_init(&etx_cdev,&fops);
+ 
+        /*Adding character device to the system*/
+        if((cdev_add(&etx_cdev,dev,1)) < 0){
+            pr_info("Cannot add the device to the system\n");
+            goto r_class;
+        }
+ 
+        /*Creating struct class*/
+        if((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL){
+            pr_info("Cannot create the struct class\n");
+            goto r_class;
+        }
+ 
+        /*Creating device*/
+        if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL){
+            pr_info("Cannot create the Device 1\n");
+            goto r_device;
+        }
+ 
+        /*Creating a directory in /sys/kernel/ */
+        kobj_ref = kobject_create_and_add("etx_sysfs",kernel_kobj);
+ 
+        /*Creating sysfs file for etx_value*/
+        if(sysfs_create_file(kobj_ref,&etx_attr.attr)){
+                pr_err("Cannot create sysfs file......\n");
+                goto r_sysfs;
+    }
+        pr_info("Device Driver Insert...Done!!!\n");
+        return 0;
+ 
 r_sysfs:
-	kobject_put(kobj_ref);
-	sysfs_remove_file(kernel_kobj, &etx_attr.attr);
-
-err:
-        device_destroy(cls, devId);
-        class_destroy(cls);
-        unregister_chrdev_region(devId, 1);
+        kobject_put(kobj_ref); 
+        sysfs_remove_file(kernel_kobj, &etx_attr.attr);
+ 
+r_device:
+        class_destroy(dev_class);
+r_class:
+        unregister_chrdev_region(dev,1);
+        cdev_del(&etx_cdev);
         return -1;
 }
- 
-static void __exit initmodule_end(void)
+
+/*
+** Module exit function
+*/
+static void __exit etx_driver_exit(void)
 {
-	kobject_put(kobj_ref);
-	sysfs_remove_file(kernel_kobj, &etx_attr.attr);
-
-	device_destroy(cls, devId);
-	class_destroy(cls);
-	unregister_chrdev_region(devId, 1);
-	printk(KERN_ALERT "initmodule release\n");
+        kobject_put(kobj_ref); 
+        sysfs_remove_file(kernel_kobj, &etx_attr.attr);
+        device_destroy(dev_class,dev);
+        class_destroy(dev_class);
+        cdev_del(&etx_cdev);
+        unregister_chrdev_region(dev, 1);
+        pr_info("Device Driver Remove...Done!!!\n");
 }
-
-module_init(initmodule_start);
-module_exit(initmodule_end);
+ 
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
+MODULE_DESCRIPTION("Simple Linux device driver (sysfs)");
+MODULE_VERSION("1.8");
