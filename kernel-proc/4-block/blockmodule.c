1401c27c04ec077a4c5250ba382200ae79d0a370
kevin.wang <kevin@wisbo.com>
2022-03-14 15:51:09 +0800

    修正块设备驱动

--- kernel-proc/4-block/blockmodule.c
+++ kernel-proc/4-block/blockmodule.c
@@ -1,178 +1,114 @@
-// 快设备的例子
-//https://www.xuebuyuan.com/3227084.html
-//https://blog.csdn.net/cxy_chen/article/details/80998510
+//https://blog.cyyself.name/linux-block-device-drvier-ramdisk/
 
-#include<linux/init.h>    //初始换函数
-#include<linux/kernel.h>  //内核头文件
-#include<linux/module.h>  //模块的头文件
-#include<linux/blkdev.h>  //块设备的相关函数
-#include <linux/hdreg.h>  //struct hd_geometry
-#include<linux/vmalloc.h> //申请内存
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/blkdev.h>
+#include <linux/blk-mq.h>
+#include <linux/vmalloc.h>
+
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("cyy");
+MODULE_DESCRIPTION("Simple Ramdisk Module");
 
-#define SIMP_BLKDEV_DISKNAME        "ramblkdev"
-#define SIMP_BLKDEV_DEVICEMAJOR     COMPAQ_SMART2_MAJOR
-#define SIMP_BLKDEV_BYTES           (32*1024*1024)
-#define SIMP_BLKDEV_MAXPARTITIONS   (1)  // 分区数目
+int myramdisk_major = 0;
 
-// 一个扇区512个字节 >> 9
+#define myramdisk_ndev 2
+const int myramdisk_dev_size = 16*1024*1024;//16M
+#define KERNEL_SECTOR_SIZE 512
 
-static struct gendisk *simp_blkdev_disk;
-static struct request_queue *simp_blkdev_queue;
-static blk_qc_t simp_blkdev_do_request(struct request_queue *q, struct bio *bio);
+struct myramdisk_dev {
+	unsigned char *data;
+	struct request_queue *queue;
+	struct gendisk *gd;
+	struct blk_mq_tag_set tag_set;
+} myramdisk_devs[myramdisk_ndev];
 
-unsigned char *simp_blkdev_data;
-
-static int simp_blkdev_getgeo(struct block_device *bdev,
-                struct hd_geometry *geo)
-{
-        /*
-         * capacity        heads        sectors        cylinders
-         * 0~16M        1        1        0~32768
-         * 16M~512M        1        32        1024~32768
-         * 512M~16G        32        32        1024~32768
-         * 16G~...        255        63        2088~...
-         */
-        if (SIMP_BLKDEV_BYTES < 16 * 1024 * 1024) {
-                geo->heads = 1;
-                geo->sectors = 1;
-
-        } else if (SIMP_BLKDEV_BYTES < 512 * 1024 * 1024) {
-                geo->heads = 1;
-                geo->sectors = 32;
-        } else if (SIMP_BLKDEV_BYTES < 16ULL * 1024 * 1024 * 1024) {
-                geo->heads = 32;
-                geo->sectors = 32;
-        } else {
-                geo->heads = 255;
-                geo->sectors = 63;
-        }
-
-        geo->cylinders = SIMP_BLKDEV_BYTES>>9/geo->heads/geo->sectors;
-
-        return 0;
-}
-
-struct block_device_operations simp_blkdev_fops = {
-          .owner   = THIS_MODULE,
-	  .getgeo  = simp_blkdev_getgeo,
+static const struct block_device_operations myramdisk_fops = {
+	.owner = THIS_MODULE,
 };
 
-static int __init blockmodule_start(void) {
-	int ret;
-	simp_blkdev_data = (unsigned char*)vmalloc(SIMP_BLKDEV_BYTES);
-	if ( !simp_blkdev_data ) {
-		ret = -ENOMEM;
-		goto err_init_queue;
+static blk_status_t myramdisk_queue_rq(struct blk_mq_hw_ctx *hctx,const struct blk_mq_queue_data* bd) {
+	blk_status_t ret = BLK_STS_OK;
+	struct request *req = bd->rq;
+	struct myramdisk_dev *dev = req->rq_disk->private_data;
+	sector_t rq_pos = blk_rq_pos(req);
+	blk_mq_start_request(req);
+
+	struct bio_vec bvec;
+	struct req_iterator iter;
+	rq_for_each_segment(bvec, req, iter) {
+		size_t num_sector = blk_rq_cur_sectors(req);
+		unsigned char *buffer = page_address(bvec.bv_page) + bvec.bv_offset;
+		unsigned long offset = rq_pos * KERNEL_SECTOR_SIZE;
+		if ((offset + num_sector*KERNEL_SECTOR_SIZE) <= myramdisk_dev_size) {//avoid buffer overflow
+			if (rq_data_dir(req) == WRITE) memcpy(dev->data+offset,buffer,num_sector*KERNEL_SECTOR_SIZE);
+			else memcpy(buffer,dev->data+offset,num_sector*KERNEL_SECTOR_SIZE);
+		}
+		else {
+			ret = BLK_STS_IOERR;
+			goto end;
+		}
+		rq_pos += num_sector;
 	}
-
-	//simp_blkdev_queue = blk_init_queue(simp_blkdev_do_request, NULL);
-	simp_blkdev_queue = blk_alloc_queue(simp_blkdev_do_request, NUMA_NO_NODE);
-	if (!simp_blkdev_queue) {
-		ret = -ENOMEM;
-		goto err_init_queue;
-	}
-	//blk_queue_make_request(simp_blkdev_queue, simp_blkdev_make_request);
-
-	// 申请一个块设备
-	simp_blkdev_disk = alloc_disk(SIMP_BLKDEV_MAXPARTITIONS);
-	if (!simp_blkdev_disk) {
-		ret = -ENOMEM;
-		goto err_alloc_disk;
-	}
-
-	strcpy(simp_blkdev_disk->disk_name, SIMP_BLKDEV_DISKNAME);
-	simp_blkdev_disk->major = SIMP_BLKDEV_DEVICEMAJOR;
-	simp_blkdev_disk->first_minor = 0;
-	simp_blkdev_disk->fops = &simp_blkdev_fops;
-	simp_blkdev_disk->queue = simp_blkdev_queue;
-	// 以扇区为单位,因此要>>9
-	set_capacity(simp_blkdev_disk, SIMP_BLKDEV_BYTES>>9);
-
-	// 将块设备的结构体添加到系统中
-	add_disk(simp_blkdev_disk);
-    	printk(KERN_ALERT "Loading blockmodule...\n");
-    	return 0;
-
-err_alloc_disk:
-        blk_cleanup_queue(simp_blkdev_queue);
-err_init_queue:
-        return ret;
-}
- 
-static void __exit blockmodule_end(void)
-{
-	del_gendisk(simp_blkdev_disk);
-	put_disk(simp_blkdev_disk);
-	blk_cleanup_queue(simp_blkdev_queue);
-	if (!simp_blkdev_data)
-		vfree(simp_blkdev_data);
-    	printk(KERN_ALERT "blockmodule release\n");
+end:
+	blk_mq_end_request(req,ret);
+	return ret;
 }
 
-//static void simp_blkdev_do_request(struct request_queue *q)
-static blk_qc_t simp_blkdev_do_request(struct request_queue *q, struct bio *bio)
-{
-	return BLK_QC_T_NONE;
-	/*
-        struct request *req;
-	struct bio *req_bio;// 当前请求的bio
-	struct bio_vec *bvec;// 当前请求的bio的段(segment)链表
+static const struct blk_mq_ops myramdisk_mq_ops = {
+	.queue_rq = myramdisk_queue_rq,
+};
 
-	char *disk_mem;      // 需要读/写的磁盘区域
-	char *buffer;        // 磁盘块设备的请求在内存中的缓冲区
-	int i = 0;
-
-        while ((req = blk_fetch_request(q)) != NULL) {
-                if ((blk_rq_pos(req) + blk_rq_cur_sectors(req)) << 9
-                        > SIMP_BLKDEV_BYTES) {
-                        printk(KERN_ERR SIMP_BLKDEV_DISKNAME
-                                ": bad request: block=%llu, count=%u\n",
-                                (unsigned long long)blk_rq_pos(req),
-                                blk_rq_cur_sectors(req));
-                        blk_end_request_all(req, -EIO);
-                        continue;
-                }
-
-
-		disk_mem = simp_blkdev_data + (blk_rq_pos(req) << 9);
-		req_bio = req->bio;// 获取当前请求的bio
-
-                switch (rq_data_dir(req)) {
-                case READ:
-			while(req_bio != NULL) {
-				//　for循环处理bio结构中的bio_vec结构体数组（bio_vec结构体数组代表一个完整的缓冲区）
-				for( i=0; i<req_bio->bi_vcnt; i++ ) {
-					bvec = &(req_bio->bi_io_vec[i]);
-					buffer = kmap(bvec->bv_page) + bvec->bv_offset;
-					memcpy(buffer, disk_mem, bvec->bv_len);
-					kunmap(bvec->bv_page);
-					disk_mem += bvec->bv_len;
-				}
-				req_bio = req_bio->bi_next;
-			}
-                        __blk_end_request_all(req, 0);
-                        break;
-                case WRITE:
-			while(req_bio != NULL) {
-				for(i=0; i<req_bio->bi_vcnt; i++) {
-					bvec = &(req_bio->bi_io_vec[i]);
-					buffer = kmap(bvec->bv_page) + bvec->bv_offset;
-					memcpy(disk_mem, buffer, bvec->bv_len);
-					kunmap(bvec->bv_page);
-					disk_mem += bvec->bv_len;
-				}
-				req_bio = req_bio->bi_next;
-			}
-                        __blk_end_request_all(req, 0);
-                        break;
-                default:
-                        //No default because rq_data_dir(req) is 1 bit 
-                        break;
-                }
-        }
-	*/
+static void myramdisk_add_device(struct myramdisk_dev *dev,int first_minor) {
+	//alloc memory for ramdisk data
+	dev->data = vmalloc(myramdisk_dev_size);
+	printk(KERN_INFO "alloc memory ok addr=%llx\n",dev->data);
+	//init mq
+	dev->queue = blk_mq_init_sq_queue(&dev->tag_set,&myramdisk_mq_ops,128,BLK_MQ_F_SHOULD_MERGE);
+	blk_queue_logical_block_size(dev->queue,KERNEL_SECTOR_SIZE);
+	dev->queue->queuedata = dev;
+	printk(KERN_INFO "init mq ok\n");
+	//init gendisk
+	dev->gd = alloc_disk(myramdisk_major);
+	dev->gd->major = myramdisk_major;
+	dev->gd->first_minor = first_minor;
+	dev->gd->fops = &myramdisk_fops;
+	dev->gd->private_data = dev;
+	dev->gd->queue = dev->queue;
+	sprintf(dev->gd->disk_name,"myramdisk%d",first_minor);//the filename in /dev/
+	set_capacity(dev->gd,myramdisk_dev_size / KERNEL_SECTOR_SIZE);
+	printk(KERN_INFO "init gendisk ok\n");
+	//add disk
+	add_disk(dev->gd);
+	printk(KERN_INFO "add disk ok\n");
 }
 
-module_init(blockmodule_start);
-module_exit(blockmodule_end);
+static int __init myramdisk_init(void) {
+	myramdisk_major = register_blkdev(0,"myramdisk");
+	if (myramdisk_major < 0) {
+		printk(KERN_WARNING "register_blkdev myramdisk failed.\n");
+	}
+	else {
+		printk(KERN_INFO "register_blkdev myramdisk succeed, major = %d.\n",myramdisk_major);
+		int i;
+		for (i=0;i<myramdisk_ndev;i++) 
+			myramdisk_add_device(&myramdisk_devs[i],i*16);
+	}
+	return 0;
+}
+
+static void __exit myramdisk_exit(void) {
+	int i;
+	for (i=0;i<myramdisk_ndev;i++) {
+		if (myramdisk_devs[i].data) vfree(myramdisk_devs[i].data);
+		if (myramdisk_devs[i].queue) blk_cleanup_queue(myramdisk_devs[i].queue);
+		if (myramdisk_devs[i].gd) {
+			del_gendisk(myramdisk_devs[i].gd);
+			put_disk(myramdisk_devs[i].gd);
+		}
+	}
+	unregister_blkdev(myramdisk_major,"myramdisk");
+}
+
+module_init(myramdisk_init);
+module_exit(myramdisk_exit);
