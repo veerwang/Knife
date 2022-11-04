2fc13010be98ddade85f73c255d06fc28bdb121d
kevin <kevin.wang2004@hotmail.com>
2022-11-04 21:58:08 +0800

    整理代码

--- cpp_11_14_17_20_test/8-final-override/main.cpp
+++ cpp_11_14_17_20_test/8-final-override/main.cpp
@@ -3,71 +3,64 @@
  *
  *       Filename:  main.cpp
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
- *        Created:  2020年06月28日 
- *       Revision:  1 
+ *        Created:  2020年06月28日
+ *       Revision:  1
  *       Compiler:  gcc
  *
- *         Author:  kevin.wang 
- *   Organization:  
+ *         Author:  kevin.wang
+ *   Organization:
  *
  * =====================================================================================
  */
 
-#include 	<cstdlib>
-#include 	<cstdio>
-#include 	<iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 
 class object {
-public:
-	object () {;}
-	object (const object&) {;}
-	virtual ~object() {;}
+   public:
+    object() { ; }
+    object(const object &) { ; }
+    virtual ~object() { ; }
 
-	virtual void display() final {
-		std::cout << "object display" << std::endl;
-	}
+    virtual void display() final { std::cout << "object display" << std::endl; }
 
-	virtual void trigger() {
-		std::cout << "trigger in object" << std::endl;
-	}
-protected:
-	
-private:
-	
+    virtual void trigger() { std::cout << "trigger in object" << std::endl; }
+
+   protected:
+   private:
 };
 
 class circle : public object {
-public:
-	circle () {;}
-	circle (const circle&) {;}
-	virtual ~circle() {;}
+   public:
+    circle() { ; }
+    circle(const circle &) { ; }
+    virtual ~circle() { ; }
 
-	/* 注意基类中的对应函数必须存在，否则不能用override进行限定 */
-	virtual void trigger() override {
-		std::cout << "trigger in circle" << std::endl;
-	}
+    /* 注意基类中的对应函数必须存在，否则不能用override进行限定 */
+    virtual void trigger() override {
+        std::cout << "trigger in circle" << std::endl;
+    }
 
-	/* 由于基类的final属性的存在，因此不能进行虚函数的改写 */
-	/*
-	virtual void display() {
-		std::cout << "circle display" << std::endl;
-	}
-	*/
-protected:
-	
-private:
-	
+    /* 由于基类的final属性的存在，因此不能进行虚函数的改写 */
+    /*
+    virtual void display() {
+            std::cout << "circle display" << std::endl;
+    }
+    */
+   protected:
+   private:
 };
 
 int main(int argc, const char *argv[]) {
-	std::cout << "template v1.0.0"  << std::endl;	
+    std::cout << "template v1.0.0" << std::endl;
 
-	circle c;
-	c.display();
-	c.trigger();
+    circle c;
+    c.display();
+    c.trigger();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
