746906a672fa08235d4913a6b6b6caa58ba3cc2e
kevin <kevin.wang2004@hotmail.com>
2024-02-14 16:40:44 +0800

    添加cmake代码

---
+++ cmake/0-init/main.cc
@@ -0,0 +1,19 @@
+/*!
+    \file    main.cc
+    \brief   测试程序
+
+    \version 2024-02-14, V1.0
+	\author	 kevin.wang
+	\note    无 
+*/
+
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+
+int
+main(int argc, const char *argv[]) {
+	std::cout << "测试程序" << std::endl;	
+	return EXIT_SUCCESS;
+}
