47117e66648f7f1c84f1818675a3ffc34d0caab0
kevin <kevin.wang2004@hotmail.com>
2024-02-14 17:15:12 +0800

    添加多目录文件内容

---
+++ cmake/1-multi/api.cc
@@ -0,0 +1,18 @@
+/*!
+    \file    api.cc
+    \brief   工程实现
+
+    \version 2024-02-14, V1.0
+	\author	 kevin.wang
+	\note    无
+*/
+
+#include "api.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+void disp_log(void) {
+	std::cout << "display" << std::endl;
+}
---
+++ cmake/1-multi/main.cc
@@ -0,0 +1,23 @@
+/*!
+    \file    main.cc
+    \brief   测试程序
+
+    \version 2024-02-14, V1.0
+	\author	 kevin.wang
+	\note    无 
+*/
+
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+
+#include "utils/module.h"
+#include "api.h"
+
+int
+main(int argc, const char *argv[]) {
+	std::cout << "测试程序: " << module_version() << std::endl;	
+	disp_log();
+	return EXIT_SUCCESS;
+}
