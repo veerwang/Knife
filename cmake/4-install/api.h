20ffbc9715a59dd59e4488261d96ce89ead985b4
kevin <kevin.wang2004@hotmail.com>
2024-02-17 12:18:58 +0800

    添加库文件install的功能

---
+++ cmake/4-install/api.h
@@ -0,0 +1,10 @@
+/*!
+    \file    api.h
+    \brief   工程对外接口
+
+    \version 2024-02-14, V1.0
+	\author	 kevin.wang
+	\note    无
+*/
+
+void disp_log(const char*);
---
+++ cmake/4-install/utils/module.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDED_MODULE_H
+#define INCLUDED_MODULE_H
+
+/*!
+    \file    module.h
+    \brief   模块文件
+
+    \version 2024-02-14, V1.0
+	\author	 kevin.wang
+	\note    无
+*/
+
+int module_version(void);
+
+#endif /* INCLUDED_MODULE_H */
